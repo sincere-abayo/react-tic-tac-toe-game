293bea43a5b85ccc87c446465b6e923b77a6a469
sincere-abayo <136345406+sincere-abayo@users.noreply.github.com>
2025-02-19 08:13:21 +0000

    ggg

--- src/App.js
+++ src/App.js
@@ -53,34 +53,14 @@ function Board({ xIsNext, squares, onPlay }) {
 }
 
 export default function Game() {
+  const [xIsNext, setXIsNext] = useState(true);
   const [history, setHistory] = useState([Array(9).fill(null)]);
-  const [currentMove, setCurrentMove] = useState(0);
-  const xIsNext = currentMove % 2 === 0;
-  const currentSquares = history[currentMove];
+  const currentSquares = history[history.length - 1];
 
   function handlePlay(nextSquares) {
-    const nextHistory = [...history.slice(0, currentMove + 1), nextSquares];
-    setHistory(nextHistory);
-    setCurrentMove(nextHistory.length - 1);
+    setHistory([...history, nextSquares]);
+    setXIsNext(!xIsNext);
   }
-
-  function jumpTo(nextMove) {
-    setCurrentMove(nextMove);
-  }
-
-  const moves = history.map((squares, move) => {
-    let description;
-    if (move > 0) {
-      description = 'Go to move #' + move;
-    } else {
-      description = 'Go to game start';
-    }
-    return (
-      <li key={move}>
-        <button onClick={() => jumpTo(move)}>{description}</button>
-      </li>
-    );
-  });
 
   return (
     <div className="game">
@@ -88,7 +68,7 @@ export default function Game() {
         <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay} />
       </div>
       <div className="game-info">
-        <ol>{moves}</ol>
+        <ol>{/*TODO*/}</ol>
       </div>
     </div>
   );
